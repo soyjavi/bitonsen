f8cd7a9c94f9b3e35fec541244d65cd256f851b4
JΛVI <hello@soyjavi.com>
2021-12-29 09:14:38 +0700

    Mockup platforms

--- src/components/Platforms/Platforms.jsx
+++ src/components/Platforms/Platforms.jsx
@@ -1,1 +1,19 @@
-export const Platforms = () => <></>;
+export const Platforms = () => (
+  <div class="content cols">
+    <div class="col">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos amet quod ipsum soluta architecto. Voluptatem
+      libero temporibus modi incidunt repudiandae natus doloribus cupiditate distinctio, sequi eaque quos corporis quo
+      illo!
+    </div>
+    <div class="col">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos amet quod ipsum soluta architecto. Voluptatem
+      libero temporibus modi incidunt repudiandae natus doloribus cupiditate distinctio, sequi eaque quos corporis quo
+      illo!
+    </div>
+    <div class="col">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos amet quod ipsum soluta architecto. Voluptatem
+      libero temporibus modi incidunt repudiandae natus doloribus cupiditate distinctio, sequi eaque quos corporis quo
+      illo!
+    </div>
+  </div>
+);
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import ReactDOM from 'react-dom';
 import { Calculator, Platforms, Spreadsheet } from './components';
 
 ReactDOM.render(<Calculator />, document.getElementById('calculator'));
-ReactDOM.render(<Platforms />, document.getElementById('platforms'));
+// ReactDOM.render(<Platforms />, document.getElementById('platforms'));
 ReactDOM.render(<Spreadsheet />, document.getElementById('spreadsheet'));
